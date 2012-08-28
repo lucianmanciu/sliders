a04bc80d144310457fc57b630eaa5759d6f12f62
lucianmanciu <lucianmanciu@gmail.com>
2012-08-28 11:13:19 +0300

    added temporary output text to scene

--- cylinder.cpp
+++ cylinder.cpp
@@ -14,8 +14,10 @@ QRectF Cylinder::boundingRect() const
 void Cylinder::paint(QPainter *painter, const QStyleOptionGraphicsItem *, QWidget *)
 {
     painter->drawLine(-90, -120, -90, 120);
+    painter->drawLine(-90, -120, -85, -120);
     painter->drawLine(-90, 120, 40, 120 );
     painter->drawLine(40, 120, 40, -120);
+    painter->drawLine(40, -120, 35, -120);
 
     painter->setBrush(QColor(0, 0, 0));
 
@@ -37,4 +39,19 @@ void Cylinder::paint(QPainter *painter, const QStyleOptionGraphicsItem *, QWidge
 
     dottedLine = new QLine(40, 0, 120, 0);
     painter->drawLine(*dottedLine);
+
+    QPoint vPoint(-145, 0);
+    QPoint wPoint(100, -150);
+    QPoint tPoint(-10, 135);
+    QPoint xPoint(90, -5);
+    QPoint yPoint(90, 15);
+    QPoint qPoint(100, 150);
+
+//Qt::TextWordWrap,
+    painter->drawText(vPoint, "v");
+    painter->drawText(wPoint, "w");
+    painter->drawText(tPoint, "t");
+    painter->drawText(xPoint, "x");
+    painter->drawText(yPoint, "y");
+    painter->drawText(qPoint, "q");
 }

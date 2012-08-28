737f7e846cd4203b4557f6cc85fdca49baf1bb87
lucianmanciu <lucianmanciu@gmail.com>
2012-08-28 10:22:53 +0300

    created QPainterPath for piston lines, QLine for dotted line

--- cylinder.cpp
+++ cylinder.cpp
@@ -13,18 +13,32 @@ QRectF Cylinder::boundingRect() const
 
 void Cylinder::paint(QPainter *painter, const QStyleOptionGraphicsItem *, QWidget *)
 {
-//    painter->drawRoundedRect(-10, -10, 20, 20, 5, 5);
+//cylinder outline
     painter->drawLine(-90, -120, -90, 120);
     painter->drawLine(-90, 120, 40, 120 );
     painter->drawLine(40, 120, 40, -120);
-    painter->drawLine(-90, 0, 40, 0);
-    painter->drawLine(-25, -300, -25 , 0);
-    painter->drawLine(80, 0, 80, -60);
-    painter->drawLine(80, 0, 80, 60);
-    painter->setPen(Qt::DotLine);
-    painter->drawLine(40, 0, 120, 0);
 
-//    QPainterPath path;
+    painter->setBrush(QColor(0, 0, 0));
+
+    QPainterPath piston;
+    piston.moveTo(-90, 0);
+    piston.lineTo(40, 0);
+    piston.moveTo(-25, 0);
+    piston.lineTo(-25, -300);
+    piston.moveTo(80, -60);
+    piston.lineTo(80, 60);
+    piston.moveTo(75, -10);
+    piston.lineTo(85, 10);
+    piston.lineTo(75, 10);
+    piston.lineTo(85, -10);
+    piston.lineTo(75, -10);
+
+    painter->drawPath(piston);
+
+    QLine dotLine(40, 0, 120, 0);
+    painter->setPen(Qt::DotLine);
+    painter->drawLine(dotLine);
+
 //    path.addRect(20, 20, 60, 60);
 
 //    path.moveTo(0, 0);
